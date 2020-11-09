6fc05cd8a6458297e259e9c99fdb34430271b0d0
Richard Ng <richardcrng@gmail.com>
2020-11-09 10:41:28 +0000

    Comment vault script

--- vaultify.sh
+++ vaultify.sh
@@ -3,8 +3,10 @@
 # Get starting directory
 starting_dir=$(pwd)
 
-# Move
+# Move markdown files to vault
 mv *.md "$starting_dir/vault"
+
+# Move README back
 cd ./vault
-mv README.md starting_dir
-cd starting_dir
+mv README.md $starting_dir
+cd $starting_dir
